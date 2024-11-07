d5b4e83e3558cf4ff2c36029809b650bd484d0d3
JSH-Tech <josueabots@gmail.com>
2024-11-07 17:40:45 -0500

    Ajout classe ticket

--- SGT_4/Models/Tickets.cs
+++ SGT_4/Models/Tickets.cs
@@ -8,5 +8,14 @@ namespace SGT_4.Models
 {
     internal class Tickets
     {
+        private int id;
+        private string? titre;
+        private enum Type{ Panne, Service}
+        private enum Categorie{ Materiel, Logiciel, Reseau}
+        private enum Priorite{ Basse, Moyenne, Haute, critique}
+        private enum Statut{ Ouvert, Ferme}
+        private string? commentaire;
+        private DateOnly dateCreation;
+        private DateOnly dateFertmeture;
     }
 }
