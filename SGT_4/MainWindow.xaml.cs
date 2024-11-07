40ca122f587cf06a2ce38ae87fe82b15c2b9f323
JSH-Tech <josueabots@gmail.com>
2024-11-07 16:17:29 -0500

    Add project files.

---
+++ SGT_4/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SGT_4
+{
+    //*****************************// 
+    // Numéro du groupe : // 
+    //Yao Josue Abotsidia 
+    //NOM_prenom2 
+    //NOM_prenom3 
+    //*******************************// 
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
