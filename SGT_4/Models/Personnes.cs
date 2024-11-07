07c6fa1eef50344f14d8b86199fee5c5d28b5746
JSH-Tech <josueabots@gmail.com>
2024-11-07 18:43:28 -0500

    Fin d'implementation des differentes classes

---
+++ SGT_4/Models/Personnes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGT_4.Models
+{
+    internal class Personnes
+    {
+		private static int compteur = 1;
+		private int matricule;
+		private string? nom;
+		private string? courriel;
+		private string? departement;
+		public List<Tickets> listeTicketsUnePersonne;
+        public Personnes(string? nom, string? courriel, string? departement)
+        {
+			this.matricule = compteur;
+            this.nom = Nom;
+            this.courriel = Courriel;
+            this.departement = Departement;
+            listeTicketsUnePersonne = new List<Tickets>();
+            compteur++;
+        }
+
+        public string? Departement
+        {
+			get { return this.departement; }
+			set { this.departement = value; }
+		}
+
+		public string? Courriel
+        {
+			get { return this.courriel; }
+			set { this.courriel = value; }
+		}
+
+
+		public string? Nom
+        {
+			get { return this.nom; }
+			set { this.nom = value; }
+		}
+
+
+		public int Matricule
+        {
+			get { return this.matricule; }
+			set { this.matricule = value; }
+		}
+
+	}
+}
--- SGT_4/Models/Tickets.cs
+++ SGT_4/Models/Tickets.cs
@@ -8,6 +8,7 @@ namespace SGT_4.Models
 {
     internal class Tickets
     {
+        private int compteur = 1;
         private int id;
         private string? titre;
         private enum Type{ Panne, Service}
@@ -17,5 +18,39 @@ namespace SGT_4.Models
         private string? commentaire;
         private DateOnly dateCreation;
         private DateOnly dateFertmeture;
+        Personnes? personne;
+        public Tickets(string? titre, string? commentaire, DateOnly dateCreation, DateOnly dateFertmeture)
+        {
+            this.id=compteur;
+            this.titre = Titre;
+            this.commentaire = Commentaire;
+            this.dateCreation = DateCreation;
+            this.dateFertmeture = DateFertmeture;
+            compteur++;
+        }
+
+        public string Titre
+        {
+            get { return this.titre; }
+            set { this.titre = value; }
+        }
+
+        public string Commentaire
+        {
+            get { return this.commentaire; }
+            set { this.commentaire = value; }
+        }
+
+        public DateOnly DateCreation
+        {
+            get { return this.dateCreation; }
+            set { this.dateCreation = value; }
+        }
+
+        public DateOnly DateFertmeture
+        {
+            get { return this.dateFertmeture; }
+            set { this.dateFertmeture = value; }
+        }
     }
 }
